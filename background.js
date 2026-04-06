0faf008227c055d472be0e2f7d5792941ff314aa
Icy-Cat <icy.cat@qq.com>
2026-04-06 09:50:38 +0800

    fix: suppress connection error when sidepanel/tab is not open

--- background.js
+++ background.js
@@ -4,12 +4,10 @@ chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
 // Relay messages between sidePanel and content scripts
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.target === 'sidepanel') {
-    // From content/bridge → sidePanel
-    chrome.runtime.sendMessage(msg);
+    chrome.runtime.sendMessage(msg).catch(() => {});
   } else if (msg.target === 'content') {
-    // From sidePanel → content script on active tab
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0]) chrome.tabs.sendMessage(tabs[0].id, msg);
+      if (tabs[0]) chrome.tabs.sendMessage(tabs[0].id, msg).catch(() => {});
     });
   }
 });
